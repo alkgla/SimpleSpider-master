f91d234e22c630331fba00af68c59bf2d3d848ec
alkgla <1541988423@qq.com>
2016-12-10 12:57:44 +0800

    简单爬虫--BeautifulSoup

---
+++ tool/html_outputer.py
@@ -0,0 +1,40 @@
+'''
+Created on 2016骞�9鏈�8鏃�
+
+@author: Luo Qiang
+'''
+
+
+class HtmlOutputer(object):
+    
+    def __init__(self):
+        self.datas=[]
+     
+    def collect_data(self,new_data):
+        if new_data is None:
+            return
+        self.datas.append(new_data)
+    
+    def output_html(self):
+        fout = open('outputer.html','w',encoding='utf-8')
+        
+        fout.write('<html>')
+        fout.write("<head><meta http-equiv=\"content-type\" content=\"text/html;charset=utf-8\"></head>")
+        fout.write('<body>')
+        fout.write('<table>')
+
+        
+        # ascii python 默认编码
+        for data in self.datas:
+            fout.write('<tr>')
+            fout.write('<td>%s</td>' % data['url'])
+            fout.write('<td>%s</td>' % data['title'])
+            fout.write('<td>%s</td>' % data['summary'])
+            fout.write('</tr>')
+
+        fout.write('</table>')
+        fout.write('</body>')
+        fout.write('</html>')
+        
+        fout.close()
+        
